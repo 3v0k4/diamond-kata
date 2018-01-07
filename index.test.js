515dd642c3ae9f02ab2903880889e7649a5be9c6
3v0k4 <riccardo.odone@gmail.com>
2018-01-07 08:38:08 +0100

    property: last row contains A

--- index.test.js
+++ index.test.js
@@ -11,8 +11,10 @@ const debug = x => {
 const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
 const char = jsc.suchthat(jsc.asciichar, c => alphabet.includes(c))
 const firstRow = string => string.split('\n')[0]
+const lastRow = string => string.split('\n')[string.split('\n').length - 1]
 
 describe('diamond', () => {
   jsc.property('is not empty', char, c => make(c).length !== 0)
   jsc.property('first row contains A', char, c => firstRow(make(c)).trim() === 'A')
+  jsc.property('last row contains A', char, c => lastRow(make(c)).trim() === 'A')
 })
