1f632453e4f0e8c86a03573975f2079e2da876fe
3v0k4 <riccardo.odone@gmail.com>
2018-01-07 11:46:40 +0100

    (refactor) test helpers

--- index.test.js
+++ index.test.js
@@ -13,19 +13,10 @@ const alphabetUntilBefore = char => alphabet.slice(0, alphabet.indexOf(char))
 const char = jsc.suchthat(jsc.asciichar, c => alphabet.includes(c))
 const firstRow = string => string.split('\n')[0]
 const lastRow = string => string.split('\n')[string.split('\n').length - 1]
-const leading = string => {
-  const index = string.search(/[A-Z]/)
-  return string.slice(0, index)
-}
-const trailing = string => {
-  const index = string.search(/[A-Z]/)
-  return string.slice(index + 1)
-}
-const rowHasSymmetricalContour = row => {
-  const leadingElements = leading(row).length
-  const trailingElements = trailing(row).length
-  return leadingElements === trailingElements
-}
+const indexOfChar = string => string.search(/[A-Z]/)
+const leading = string => string.slice(0, indexOfChar(string))
+const trailing = string => string.slice(indexOfChar(string) + 1)
+const rowHasSymmetricalContour = row => leading(row).length === trailing(row).length
 const rowsHaveSymmetricalContour = diamond =>
   diamond.split('\n').map(rowHasSymmetricalContour).reduce((acc, x) => acc && x)
 
