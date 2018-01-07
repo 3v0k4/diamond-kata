20252db0ac1e3cf1b6786b189acd0455b73482ca
3v0k4 <riccardo.odone@gmail.com>
2018-01-07 08:24:09 +0100

    A..Z generator

--- index.test.js
+++ index.test.js
@@ -8,6 +8,9 @@ const debug = x => {
   return true;
 }
 
+const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
+const char = jsc.suchthat(jsc.asciichar, c => alphabet.includes(c))
+
 describe('diamond', () => {
-  jsc.property('is not empty', jsc.asciichar, debug)
+  jsc.property('is not empty', char, debug)
 })
